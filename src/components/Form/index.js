603a6c9e2072bcad8ce490dc89842abd02a974b8
Gustavo Teixeira <gustavofigueiredoteixeira8@gmail.com>
2021-12-25 10:19:24 -0300

    Adiciona Form como um component

---
+++ src/components/Form/index.js
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+import { primaryDarkColor } from '../../config/colors';
+
+export const Form = styled.form`
+  width: 100%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  label {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
+    font-weight: bold;
+    width: 100%;
+    max-width: 500px;
+  }
+
+  input {
+    padding-left: 9px;
+    margin-top: 6px;
+    border-radius: 3px;
+    border: 1px solid ${primaryDarkColor};
+    height: 40px;
+    font-size: 17px;
+  }
+
+  button {
+    padding: 10px 80px;
+  }
+
+  input:focus {
+    box-shadow: 0px 0px 7px 0px ${primaryDarkColor};
+  }
+
+  small {
+    margin-top: 5px;
+  }
+
+  a {
+    color: black;
+    font-size: 14px;
+  }
+
+  .divisor {
+    margin: 0 8px;
+  }
+`;
