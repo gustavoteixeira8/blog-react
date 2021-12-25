959ca8de8efeba9658c8f6a1602da079d1262379
Gustavo Teixeira <gustavofigueiredoteixeira8@gmail.com>
2021-12-25 10:19:52 -0300

    Adiciona pagina de envio de email de verificação

---
+++ src/pages/SendEmailVerification/index.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import { Container } from '../../styles/globalStyles';
+import { Form } from '../../components/Form';
+import { Button } from '../../components/Button';
+import { Loading } from '../../components/Loading';
+import { Title } from './styled';
+import axios from '../../services/axios';
+import { toast } from 'react-toastify';
+import { browserHistory } from '../../services/browserHistory';
+
+export const SendEmailVerification = () => {
+  const [email, setEmail] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!email) {
+      toast.error('Email must be provided');
+      return;
+    }
+
+    try {
+      setIsLoading(true);
+      await axios.post('/auth/email/verify', { email });
+
+      setIsLoading(false);
+      toast.success(
+        'If the email exists in the database and is not verified, you will receive an email',
+      );
+
+      browserHistory.push('/login');
+    } catch (error) {
+      toast.error('Internal error, try again later');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <Loading isLoading={isLoading} />
+      <Container>
+        <Title>
+          Email verification
+          <span className="border-title"></span>
+        </Title>
+
+        <Form onSubmit={handleSubmit}>
+          <label htmlFor="login">
+            Email
+            <input
+              name="email"
+              placeholder="Type your email"
+              type="email"
+              autoFocus
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+
+          <Button type="submit">Submit</Button>
+        </Form>
+      </Container>
+    </>
+  );
+};
---
+++ src/pages/SendEmailVerification/styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  margin-bottom: 40px;
+  cursor: pointer;
+  text-align: center;
+`;
