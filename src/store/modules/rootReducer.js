39be20a20a416ad2679fb7a395a9c0b2b144ada5
Gustavo Teixeira <gustavofigueiredoteixeira8@gmail.com>
2021-12-28 15:36:33 -0300

    Adiciona userReducer

--- src/store/modules/rootReducer.js
+++ src/store/modules/rootReducer.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
 import { authReducer } from './auth/reducer';
+import { userReducer } from './user/reducer';
 
-export const rootReducer = combineReducers({ auth: authReducer });
+export const rootReducer = combineReducers({ auth: authReducer, user: userReducer });
