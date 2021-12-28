69650e989b5dd8f3657ffe151f5a689d0ac95467
Gustavo Teixeira <gustavofigueiredoteixeira8@gmail.com>
2021-12-28 15:37:04 -0300

    Adiciona userSaga

--- src/store/modules/rootSaga.js
+++ src/store/modules/rootSaga.js
@@ -1,6 +1,7 @@
 import { all } from 'redux-saga/effects';
 import { authSaga } from './auth/saga';
+import { userSaga } from './user/saga';
 
 export const rootSaga = function* () {
-  return yield all([authSaga]);
+  return yield all([authSaga, userSaga]);
 };
