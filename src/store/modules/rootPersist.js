22e086a23ad20a31880cc3f3630c99a8712253bc
Gustavo Teixeira <gustavofigueiredoteixeira8@gmail.com>
2021-12-28 15:36:05 -0300

    Adiciona encryptTransform

--- src/store/modules/rootPersist.js
+++ src/store/modules/rootPersist.js
@@ -1,12 +1,23 @@
-import storage from 'redux-persist/lib/storage';
+import storage from 'redux-persist/lib/storage/session';
 import { persistReducer } from 'redux-persist';
+import { encryptTransform } from 'redux-persist-transform-encrypt';
+import { generateRandomString } from '../../utils/generateRandomString';
 
 export const rootPersist = (reducers) => {
   const persistedReducers = persistReducer(
     {
       key: 'blog',
       storage,
-      whitelist: ['auth'],
+      whitelist: ['auth', 'user'],
+      transforms: [
+        encryptTransform({
+          secretKey: generateRandomString(),
+          onError() {
+            dispatchEvent('storage');
+          },
+        }),
+      ],
+      serialize: true,
     },
     reducers,
   );
