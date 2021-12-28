70775ab374ed6502b1cc85eb1040172df4021353
Gustavo Teixeira <gustavofigueiredoteixeira8@gmail.com>
2021-12-28 15:25:06 -0300

    Adiciona responseType e timeout

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,4 +1,10 @@
 import axios from 'axios';
+// import serialize from 'serialize-javascript';
 import { appConfig } from '../config/app';
 
-export default axios.create({ baseURL: appConfig.apiUrl, timeoutErrorMessage: 1000 * 20 });
+export default axios.create({
+  baseURL: appConfig.apiUrl,
+  timeout: 1000 * 10,
+  responseType: 'json',
+  // transformRequest: [(data) => JSON.stringify(serialize(data))],
+});
