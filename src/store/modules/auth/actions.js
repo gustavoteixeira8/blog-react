5e5e118d2995a1e4451e58f3c628c3d82d857c11
Gustavo Teixeira <gustavofigueiredoteixeira8@gmail.com>
2021-12-28 15:33:08 -0300

    Adiciona action de logout

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,5 +1,6 @@
 import * as types from '../types';
 
+export const createLogoutRequest = (payload) => ({ type: types.LOGOUT_REQUEST, payload });
 export const createLoginRequest = (payload) => ({ type: types.LOGIN_REQUEST, payload });
 export const createLoginSuccess = (payload) => ({ type: types.LOGIN_SUCCESS, payload });
 export const createLoginError = (payload) => ({ type: types.LOGIN_ERROR, payload });
