c5a12712d874034ff74ee184faf2e3d98688a89e
Gustavo Teixeira <gustavofigueiredoteixeira8@gmail.com>
2021-12-29 19:42:12 -0300

    Adiciona footer globalmente no site

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import { Router } from 'react-router-dom';
-import { GlobalStyles } from './styles/globalStyles';
+import { GlobalStyles, Wrapper } from './styles/globalStyles';
 import { Header } from './components/Header';
 import { AppRoutes } from './routes';
 import { GlobalEvents } from './components/GlobalEvents';
@@ -9,27 +9,32 @@ import { browserHistory } from './services/browserHistory';
 import { Provider } from 'react-redux';
 import { store, persistor } from './store';
 import { PersistGate } from 'redux-persist/integration/react';
+import { Footer } from './components/Footer';
 
 const App = function () {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <Router history={browserHistory} location={browserHistory.location}>
-          <GlobalEvents />
-          <Header />
-          <GlobalStyles />
-          <AppRoutes />
-          <ToastContainer
-            autoClose={5000}
-            className="toast-container"
-            pauseOnFocusLoss={true}
-            theme="dark"
-            toastClassName={Math.random()}
-            closeOnClick
-            limit={6}
-            closeButton
-            draggable
-          />
+          <Wrapper>
+            <GlobalEvents />
+            <Header />
+            <GlobalStyles />
+            <AppRoutes />
+            <ToastContainer
+              autoClose={5000}
+              className="toast-container"
+              pauseOnFocusLoss={true}
+              theme="dark"
+              toastClassName={Math.random()}
+              closeOnClick
+              limit={6}
+              closeButton
+              draggable
+            />
+          </Wrapper>
+
+          <Footer />
         </Router>
       </PersistGate>
     </Provider>
