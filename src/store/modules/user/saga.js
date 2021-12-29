5a298c1a24e4dddb4f93a0ba2d4bc34fefa45f8d
Gustavo Teixeira <gustavofigueiredoteixeira8@gmail.com>
2021-12-29 19:01:53 -0300

    Adiciona deleteUserLoggedInRequest

--- src/store/modules/user/saga.js
+++ src/store/modules/user/saga.js
@@ -3,6 +3,7 @@ import { toast } from 'react-toastify';
 import { all, call, put, takeLatest } from 'redux-saga/effects';
 import axios from '../../../services/axios';
 import * as userActions from '../user/actions';
+import * as authActions from '../auth/actions';
 import * as types from '../types';
 
 export const fetchUserLoggedInRequest = function* () {
@@ -48,7 +49,28 @@ export const updateUserLoggedInRequest = function* ({ payload }) {
   }
 };
 
+export const deleteUserLoggedInRequest = function* () {
+  try {
+    const response = yield call(axios.delete, '/user');
+
+    const message = get(response, 'data.body.message', 'Your user was deleted successfully');
+
+    toast.success(message);
+
+    yield put(authActions.createLogoutRequest());
+  } catch (error) {
+    const errors = get(error, 'response.data.body.errors', []);
+    const status = get(error, 'response.data.body.status', 500);
+
+    if (status >= 400 || status <= 499) {
+      errors.map((error) => toast.error(error, { toastId: Math.random() }));
+      return;
+    }
+  }
+};
+
 export const userSaga = all([
   takeLatest(types.FETCH_USER_LOGGED_IN_REQUEST, fetchUserLoggedInRequest),
   takeLatest(types.UPDATE_USER_LOGGED_IN_REQUEST, updateUserLoggedInRequest),
+  takeLatest(types.DELETE_USER_LOGGED_IN_REQUEST, deleteUserLoggedInRequest),
 ]);
