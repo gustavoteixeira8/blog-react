44d23c124fd21622407a4d0729679cc56639edc6
Gustavo Teixeira <gustavofigueiredoteixeira8@gmail.com>
2021-12-29 18:55:32 -0300

    Adiciona pagina de DeleteAccount

---
+++ src/pages/DeleteAccount/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Container } from '../../styles/globalStyles';
+import { Title } from '../Dashboard/styled';
+import { DeleteAccountBox } from './styled';
+import { Button } from '../../components/Button';
+import { useDispatch } from 'react-redux';
+import * as userActions from '../../store/modules/user/actions';
+
+export const DeleteAccount = () => {
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(userActions.createDeleteUserLoggedInRequest());
+  };
+
+  return (
+    <>
+      <Container>
+        <DeleteAccountBox>
+          <Title>Delete my account</Title>
+
+          <p>
+            When you delete your account all your articles will be deleted together. You will have
+            one month to recover it, just log in with the correct credentials.
+          </p>
+
+          <Button onClick={handleClick} type="button">
+            DELETE MY ACCOUNT
+          </Button>
+        </DeleteAccountBox>
+      </Container>
+    </>
+  );
+};
---
+++ src/pages/DeleteAccount/styled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const DeleteAccountBox = styled.div`
+  button {
+    width: 100%;
+    height: 80px;
+    font-size: 30px;
+    margin-top: 20px;
+  }
+
+  p {
+    font-size: 22px;
+    text-align: center;
+  }
+`;
