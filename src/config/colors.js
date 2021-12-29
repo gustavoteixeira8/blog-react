232b00040937391001f9f5d85b145e95efda5619
Gustavo Teixeira <gustavofigueiredoteixeira8@gmail.com>
2021-12-29 19:42:32 -0300

    Adiciona primaryBlueColor

--- src/config/colors.js
+++ src/config/colors.js
@@ -6,3 +6,4 @@ export const primaryGreenColor = '#4BB543';
 export const secondaryGreenColor = '#0197f6';
 export const primaryYellowColor = '#dbdb04';
 export const primaryRedColor = '#fa3527';
+export const primaryBlueColor = '#1D9BF0';
