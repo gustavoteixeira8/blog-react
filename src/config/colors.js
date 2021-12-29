4808d2713d036c69fff20e73add99e06068246cd
Gustavo Teixeira <gustavofigueiredoteixeira8@gmail.com>
2021-12-29 18:55:49 -0300

    Adiciona novas cores

--- src/config/colors.js
+++ src/config/colors.js
@@ -2,7 +2,7 @@ export const primaryDarkColor = '#2b2a2b';
 export const primaryColor = '#c3073f';
 export const primaryModalBackgroundColor = 'black';
 
-export const successColor = '#4BB543';
-export const infoColor = '#0197f6';
-export const warningColor = '#4BB543';
-export const errorColor = '#fa3527';
+export const primaryGreenColor = '#4BB543';
+export const secondaryGreenColor = '#0197f6';
+export const primaryYellowColor = '#dbdb04';
+export const primaryRedColor = '#fa3527';
