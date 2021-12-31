ed08f2ec390db48289edc8435baded33df3b90eb
Gustavo Teixeira <gustavofigueiredoteixeira8@gmail.com>
2021-12-31 11:49:01 -0300

    Adiciona propriedade para alterar tamanho do Button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,9 +1,8 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { primaryColor } from '../../config/colors';
 
 export const Button = styled.button`
   background-color: transparent;
-  border: 0;
   padding: 10px 20px;
   border-radius: 3px;
   color: ${primaryColor};
@@ -12,10 +11,29 @@ export const Button = styled.button`
   transition: 500ms all;
   font-weight: bold;
   font-size: 17px;
+  margin: 0 8px;
 
   :hover {
     background-color: ${primaryColor};
     color: white;
     /* transform: scale(1.05, 1.05); */
+    a {
+      color: white;
+    }
   }
+
+  a {
+    color: ${primaryColor};
+    font-weight: bold;
+    font-size: 17px;
+  }
+
+  ${(props) =>
+    props.big &&
+    css`
+      width: 100%;
+      height: 80px;
+      font-size: 30px;
+      margin-top: 20px;
+    `}
 `;
