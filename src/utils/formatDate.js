d3281ed871ef28ad1b8bc7ffafee67e5a4e1f6ce
Gustavo Teixeira <gustavofigueiredoteixeira8@gmail.com>
2022-01-08 11:50:21 -0300

    Adiciona formatDistanceDate

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,6 +1,10 @@
-import { format } from 'date-fns';
+import { format, formatDistanceToNow } from 'date-fns';
 import { enUS } from 'date-fns/locale';
 
 export const formatDate = (date) => {
   return format(new Date(date), 'yyyy-MM-dd HH:mm', { locale: enUS });
+};
+
+export const formatDistanceDate = (date) => {
+  return formatDistanceToNow(new Date(date), { addSuffix: true });
 };
