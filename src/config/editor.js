f6367b3b2516f370275bdc41400b892672708347
Gustavo Teixeira <gustavofigueiredoteixeira8@gmail.com>
2022-01-10 17:58:57 -0300

    Adiciona codesample_languages

--- src/config/editor.js
+++ src/config/editor.js
@@ -2,6 +2,20 @@ export const editorConfig = {
   config: {
     height: 500,
     menubar: true,
+    codesample_languages: [
+      { text: 'HTML/XML', value: 'markup' },
+      { text: 'JavaScript', value: 'javascript' },
+      { text: 'CSS', value: 'css' },
+      { text: 'PHP', value: 'php' },
+      { text: 'Ruby', value: 'ruby' },
+      { text: 'Python', value: 'python' },
+      { text: 'Java', value: 'java' },
+      { text: 'C', value: 'c' },
+      { text: 'C#', value: 'csharp' },
+      { text: 'C++', value: 'cpp' },
+      { text: 'JSON', value: 'json' },
+      { text: 'Shell', value: 'shell' },
+    ],
     plugins: [
       'advlist autolink lists link charmap print preview anchor',
       'searchreplace visualblocks code codesample fullscreen',
