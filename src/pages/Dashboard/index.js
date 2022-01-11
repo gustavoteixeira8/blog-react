74197003fb567a9c76674bdf4b41ed4c8d9c4bf3
Gustavo Teixeira <gustavofigueiredoteixeira8@gmail.com>
2022-01-11 16:30:05 -0300

    Adiciona total de categorias, artigos e usuários

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,16 +1,70 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Card, CardsContainer, CardText, CardTitle } from '../../components/Card';
 import { Button } from '../../components/Button';
 import { Container } from '../../styles/globalStyles';
 import { ContainerUtilityLinks, UtilityLinks } from './styled';
 import { Title } from '../../components/Title';
+import { Loading } from '../../components/Loading';
 import { HelmetTags } from '../../components/Helmet';
+import { get } from 'lodash';
+import axios from '../../services/axios';
 
 export const Dashboard = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [categoriesTotal, setCategoriesTotal] = useState(0);
+  const [articlesTotal, setArticlesTotal] = useState(0);
+  const [usersTotal, setUsersTotal] = useState(0);
+
+  const getUsers = async () => {
+    try {
+      const response = await axios.get('/user');
+
+      const users = get(response, 'data.body.users.data', []);
+      const maxPage = get(response, 'data.body.maxPage', 1);
+
+      setUsersTotal(users.length * maxPage);
+    } catch (error) {
+      setUsersTotal('Not found');
+    }
+  };
+
+  const getCategories = async () => {
+    try {
+      const response = await axios.get('/category');
+
+      const categories = get(response, 'data.body.data', []);
+      const maxPage = get(response, 'data.body.maxPage', 1);
+
+      setCategoriesTotal(categories.length * maxPage);
+    } catch (error) {
+      setCategoriesTotal('Not found');
+    }
+  };
+
+  const getArticles = async () => {
+    try {
+      const response = await axios.get('/article');
+
+      const articles = get(response, 'data.body.articles.data', []);
+      const maxPage = get(response, 'data.body.articles.maxPage', 1);
+
+      setArticlesTotal(articles.length * maxPage);
+    } catch (error) {
+      setArticlesTotal('Not found');
+    }
+  };
+
+  useEffect(() => {
+    if (!articlesTotal || !categoriesTotal || !usersTotal) {
+      Promise.all([getUsers(), getArticles(), getCategories()]);
+    }
+  }, [setIsLoading, articlesTotal, categoriesTotal, usersTotal]);
+
   return (
     <>
       <HelmetTags title="Dashboard" />
+      <Loading isLoading={isLoading} />
 
       <Container style={{ maxWidth: '1280px' }}>
         <Title>Dashboard</Title>
@@ -18,17 +72,17 @@ export const Dashboard = () => {
         <CardsContainer>
           <Card>
             <CardTitle>Articles</CardTitle>
-            <CardText>50</CardText>
+            <CardText>{articlesTotal}</CardText>
           </Card>
 
           <Card>
             <CardTitle>Categories</CardTitle>
-            <CardText>15</CardText>
+            <CardText>{categoriesTotal}</CardText>
           </Card>
 
           <Card>
             <CardTitle>Users</CardTitle>
-            <CardText>3</CardText>
+            <CardText>{usersTotal}</CardText>
           </Card>
         </CardsContainer>
 
